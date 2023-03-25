642dab4233580dad7358e54416a2b72ccc70bd33
Pedro <phdss04@gmail.com>
2023-03-25 20:50:27 -0300

    changing env

--- src/services/Axios.ts
+++ src/services/Axios.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const dev_url = `http://localhost:3000/`;
+const dev_url = import.meta.env.VITE_DEV_URL
 const prod_url = import.meta.env.VITE_API_URL
 
-const url = import.meta.env.VITE_NODE_ENV === "development" ? dev_url : prod_url;
+const url = import.meta.env.VITE_NODE_ENV === "development" ? dev_url : prod_url
 console.log(url)
+
 export const callApi = axios.create({
-  baseURL: url,
+  baseURL: url
 });
 
 export const fetcher = (url : string) => callApi(url).then(
