89429866e42b0ff027ca1d067a318948ada0ad92
Pedro <phdss04@gmail.com>
2023-03-25 20:41:33 -0300

    log na url da api

--- src/services/Axios.ts
+++ src/services/Axios.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const dev_url = `http://localhost:3000/`;
 const prod_url = import.meta.env.VITE_API_URL
 
-const url  = import.meta.env.VITE_NODE_ENV === "development" ? dev_url : prod_url;
-
+const url = import.meta.env.VITE_NODE_ENV === "development" ? dev_url : prod_url;
+console.log(url)
 export const callApi = axios.create({
   baseURL: url,
 });
